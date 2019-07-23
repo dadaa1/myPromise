383192d6e8bd7effa1cb860a740b8cab856d5486
大山 <1692972759@qq.com>
2019-07-23 13:33:59 +0800

    es6重构

--- myprimise.js
+++ myprimise.js
@@ -1,139 +1,138 @@
-function MyPromise(fn) {
-  this.list = [];
-  this.a=null;
-  this.isSync = 0; // 判断是不是异步的
-  fn.call(this, this.res.bind(this), this.rej.bind(this));
-}
-
-MyPromise.prototype.then = function(fn) {
-  this.list.push(fn);
-  return this;
-};
-MyPromise.prototype.res = function(value) {
-  setTimeout(function(){
-    this.isSync = 1;
-    this.a = value;
+class MyPromise {
+  constructor(func) {
+    this.list = [];
+    setTimeout(() => {
+      func(this.resolve.bind(this), this.reject.bind(this));
+    }, 0);
+  }
+  resolve(data) {
+    let msg = data;
     while (this.list.length) {
-      if (typeof this.a === "object" && this.a.constructor === MyPromise) {
-        this.a.list = this.a.list.concat(this.list);
+      if (this._isMyPromise(msg)) {
+        msg.list = msg.list.concat(this.list);
         this.list.length = 0;
       } else {
+        const func = this.list.shift();
+        if (this._isErrorCallBack(func)) {
+          continue;
+        }
         try {
-          let h = this.list.shift();
-          if (typeof h == "object" && h.name == "catch") {
-            continue;
-          }
-          this.a = h(this.a);
+          msg = func.call(null, msg);
         } catch (e) {
-          while (this.list.length) {
-            h = this.list.shift();
-            if (typeof h == "object" && h.name == "catch") {
-              h.func(e);
-              this.list.length = 0;
-              return;
-            }
-          }
-          throw "你没有设置Error处理函数哦~";
+          this.reject(e);
         }
       }
     }
-  }.bind(this),0);
-};
-
-MyPromise.prototype.rej = function(err) {
-  this.isSync = 2;
-  console.log(err);
-};
-
-MyPromise.prototype.catch = function(fn) {
-  this.list.push({ name: "catch", func: fn });
-  return this;
-};
-
-MyPromise.all=function(fns){
-  let resultList=[];
-  return new MyPromise(function(res,rej){
-    for(let i=0;i<fns.length;i++){
-      fns[i].then(function(value){
-        resultList.push({index:i,value:value});
-        if(resultList.length===fns.length){
-          res(resultList);
+  }
+  reject(error) {
+    while (this.list.length) {
+      const catchFunc = this.list.shift();
+      if (this._isErrorCallBack(catchFunc)) {
+        catchFunc.errorCallback(error);
+        break;
+      }
+    }
+    throw error;
+  }
+  then(cb) {
+    if (this._isFunction(cb)) {
+      this.list.push(cb);
+    }
+    return this;
+  }
+  catch(cb) {
+    if (this._isFunction(cb)) {
+      this.list.push({
+        errorCallback: cb
+      });
+    }
+    return this;
+  }
+  _isFunction(func) {
+    return typeof func === 'function';
+  }
+  _isErrorCallBack(func) {
+    return typeof func == 'object' && this._isFunction(func.errorCallback);
+  }
+  _isMyPromise(msg) {
+    return typeof msg === 'object' && msg.constructor === MyPromise;
+  }
+}
+MyPromise.all = function(fns) {
+  const len = fns.length;
+  let reslen = 0;
+  const result = [];
+  return new MyPromise((res, rej) => {
+    fns.forEach((el, i) => {
+      el.then(data => {
+        reslen++;
+        result[i] = data;
+        if (len === reslen) {
+          res(result);
         }
-      })
-    }
+      }).catch(e => {
+        rej(e);
+      });
+    });
   });
-}
+};
+MyPromise.race = function() {};
 
-MyPromise.race=function(fns){
-  return new MyPromise(function(res,rej){
-    for(let i=0;i<fns.length;i++){
-      fns[i].then(function(value){
-        res(value);
-      })
-    }
-  });
-}
-
+// test code
+// new MyPromise((res, rej) => {
+//   if (Math.random() > 0.5) {
+//     res(1);
+//   } else {
+//     rej('我只是一个粗哟五');
+//   }
+// })
+//   .then(data => {
+//     console.log(data);
+//     console.log('2');
+//   })
+//   .then(() => {
+//     return new MyPromise(res => {
+//       console.log('hhhhh');
+//       setTimeout(() => {
+//         console.log('哈哈哈哈哈');
+//         res('1111');
+//       }, 3000);
+//     }).then(data => {
+//       console.log(data);
+//     });
+//   })
+//   .then(() => {
+//     console.log('3');
+//   });
 let a = new MyPromise(function(res, rej) {
   setTimeout(function() {
-    console.log("第一");
-    res("第二");
+    console.log('第一');
+    res('第二');
   }, 1000);
 });
 a.then(function(value) {
-    console.log("第二:", value);
-    return "第三";
-  })
+  console.log('第二:', value);
+  return '第三';
+})
   .catch(function(err) {
-    console.log("错误：", err);
+    console.log('错误：', err);
   })
   .then(function(value) {
-    console.log("第三：", value);
-    return "第四";
+    console.log('第三：', value);
+    return '第四';
   })
   .then(function(value) {
     return new MyPromise(function(res, rej) {
       setTimeout(function() {
-        console.log("新第一:", value);
-        res("新第二");
+        console.log('新第一:', value);
+        res('新第二');
       }, 1000);
     });
   })
   .then(function(value) {
-    console.log("新第二:", value);
-    // throw new Error("awosh wocu lw ~");
+    console.log('新第二:', value);
   })
   .catch(function(err) {
-    console.log("hhhhhhhhh:", err);
+    console.log('hhhhhhhhh:', err);
   });
 
-
-function hhhh(i){
-  return new MyPromise(function(res,rej){
-    setTimeout(function(){
-      console.log(i,')))))')
-      res(i)
-    },1000)
-  })
-}
-
-function hhhb(i){
-  return new MyPromise(function(res,rej){
-    console.log(i,'hhhhb');
-    res(i);
-  })
-}
-let listAll=[];
-listAll.push(hhhb(123));
-listAll.push(hhhb(456));
-listAll.push(hhhb(789));
-let all=MyPromise.all(listAll).then(function(v){
-  console.log(v);
-})
-
-new MyPromise(function(res){
-  console.log(1)
-  res(2)
-}).then((v)=>{
-  console.log(v)
-})
